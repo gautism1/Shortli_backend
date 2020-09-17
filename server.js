51ecc0a0027ad75ea0d41a7b9c2cabd38b05160f
gauti-rcubed4761 <vipingautam.fm2@gmail.com>
2020-09-17 23:55:34 +0530

    it-works-now

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 
 
 const app=express();
- /*  var cors = require('cors');
+   var cors = require('cors');
 app.use(cors())
 
 app.use(function(req, res, next) {
@@ -14,16 +14,16 @@ app.use(function(req, res, next) {
 const connectdb =require('./config/db');
 // Database connecting
 connectdb();
-*/
+
 app.get('/',(req,res)=>
 res.send("hello world"))
-/*
+
 app.use(express.json({extended : false}));
 //defing routes
  app.use('/', require('./routes/url.js'))
 app.use('/',require('./routes/allurl.js'));
 app.use('/', require('./routes/index.js'))
-*/
+
 const port =process.env.PORT || 5000   ;
 
 app.listen(port,() => console.log(`running at port ${port}`))
