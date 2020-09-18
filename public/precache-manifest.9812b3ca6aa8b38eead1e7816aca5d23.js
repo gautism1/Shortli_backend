f6ae19c9029d0fefdd2d5dec9c3551fd83eb084b
gauti-rcubed4761 <vipingautam.fm2@gmail.com>
2020-09-18 23:28:03 +0530

    added build folder

---
+++ public/precache-manifest.9812b3ca6aa8b38eead1e7816aca5d23.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8208ba161afc1ae62e9",
+    "url": "/static/css/main.06eada02.chunk.css"
+  },
+  {
+    "revision": "f8208ba161afc1ae62e9",
+    "url": "/static/js/main.d886395b.chunk.js"
+  },
+  {
+    "revision": "2f13687a94453737e8ef",
+    "url": "/static/js/runtime~main.f0d86e62.js"
+  },
+  {
+    "revision": "25a6d4b2e5b4e0edd0a6",
+    "url": "/static/js/2.17fe833f.chunk.js"
+  },
+  {
+    "revision": "a7a0877b77cd6ac7e30e",
+    "url": "/static/js/3.3278a321.chunk.js"
+  },
+  {
+    "revision": "3a73cee85d8482cce155",
+    "url": "/static/css/4.b8a85058.chunk.css"
+  },
+  {
+    "revision": "3a73cee85d8482cce155",
+    "url": "/static/js/4.2f0aa921.chunk.js"
+  },
+  {
+    "revision": "71b69aebd119ae635925",
+    "url": "/static/js/5.cec45dad.chunk.js"
+  },
+  {
+    "revision": "9bf70850e8c7fb0893d4",
+    "url": "/static/js/6.7208db04.chunk.js"
+  },
+  {
+    "revision": "6a73829169cb5981e65b3395526a1a99",
+    "url": "/static/media/logo.6a738291.svg"
+  },
+  {
+    "revision": "4d6b58af1804f2fb9b3a9ba94d3a639d",
+    "url": "/index.html"
+  }
+];
---
+++ public/static/js/6.7208db04.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{61:function(e,n,r){"use strict";r.r(n);var a=r(0),t=r.n(a);n.default=function(){return t.a.createElement("div",{className:"error404"},t.a.createElement("h1",null," Error 404"))}}}]);
+//# sourceMappingURL=6.7208db04.chunk.js.map
