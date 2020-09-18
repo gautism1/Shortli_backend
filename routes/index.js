c5a4705306863094c2ad8650bcacfe54b2cc9250
gauti-rcubed4761 <vipingautam.fm2@gmail.com>
2020-09-18 23:17:13 +0530

    implemented  public folder

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router=express.Router();
-const urL=require( '../module/url');
+const urL=require( '../models/url');
 // GET
 //DESCRIPTION GOING TO LON/ORIGINAL url
 router.get('/:code',async (req,res) =>
